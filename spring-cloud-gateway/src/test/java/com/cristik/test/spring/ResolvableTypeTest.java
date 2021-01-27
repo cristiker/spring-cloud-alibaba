c2c7e1e23fb0958432a404c205bc9dfbb26b185b
cristik <zhenghua.ni@zhangmen.com>
2021-01-27 20:54:18 +0800

    maven dependency

---
+++ spring-cloud-gateway/src/test/java/com/cristik/test/spring/ResolvableTypeTest.java
@@ -0,0 +1,28 @@
+package com.cristik.test.spring;
+
+
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.core.ResolvableType;
+import org.springframework.util.MultiValueMap;
+
+import java.lang.reflect.Array;
+
+public class ResolvableTypeTest {
+
+    private static final Logger logger = LoggerFactory.getLogger(ResolvableTypeTest.class);
+
+    @Test
+    public void test() {
+        ResolvableType superResolvableType = ResolvableType.forClass(Array.class).getSuperType();
+        logger.info("supper:" + superResolvableType.resolveGenerics());
+    }
+
+    @Test
+    public void test2() {
+        ResolvableType formDataType = ResolvableType.forClassWithGenerics(MultiValueMap.class, String.class, String.class);
+        System.out.println(formDataType);
+    }
+
+}
