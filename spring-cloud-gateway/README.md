1c5832f6413cba0d037bf867a41f276c4cae1f8b
cristik <cristik1991@gmail.com>
2021-01-28 23:56:47 +0800

    文档

--- spring-cloud-gateway/README.md
+++ spring-cloud-gateway/README.md
@@ -1,1 +1,6 @@
-https://my.oschina.net/giegie/blog/3095354
+[Ribbon](https://my.oschina.net/giegie/blog/3095354)
+
+[新一代API网关服务](https://juejin.cn/post/6844903982599684103)
+
+
+[Spring Cloud Gateway](https://docs.spring.io/spring-cloud-gateway/docs/2.2.6.RELEASE/reference/html/)
--- spring-cloud-gateway/src/main/java/com/cristik/cloud/gateway/GatewayApplication.java
+++ spring-cloud-gateway/src/main/java/com/cristik/cloud/gateway/GatewayApplication.java
@@ -3,8 +3,6 @@ package com.cristik.cloud.gateway;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.gateway.handler.predicate.CookieRoutePredicateFactory;
-import org.springframework.cloud.gateway.support.NameUtils;
 
 /**
  * @author zhenghua.ni
